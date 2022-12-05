6c3c44e07e00ac22e649c2ce66d8fe48da18aed8
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2022-12-06 00:22:20 +0300

    fix portfolio db links

--- src/Main/Screen/Portfolio/portfolio_data.js
+++ src/Main/Screen/Portfolio/portfolio_data.js
@@ -1,88 +1,88 @@
  let data = [
     {
         name:"Four card feature section",
-        URL:"../../../../frontendmentor/four-card-feature-section/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/four-card-feature-section.jpg",
+        URL:"../portfolio/frontendmentor/four-card-feature-section/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/four-card-feature-section.jpg",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Huddle landing page",
-        URL:"../../../../frontendmentor/huddle-landing-page-with-single-introductory-section/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/huddle-landing-page-with-single-introductory-section.png",
+        URL:"../portfolio/frontendmentor/huddle-landing-page-with-single-introductory-section/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/huddle-landing-page-with-single-introductory-section.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Ping coming soon page",
-        URL:"../../../../frontendmentor/ping-single-column-coming-soon-page/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/ping-single-column-coming-soon-page.png",
+        URL:"../portfolio/frontendmentor/ping-single-column-coming-soon-page/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/ping-single-column-coming-soon-page.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Single price grid component",
-        URL:"../../../../frontendmentor/single-price-grid-component/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/single-price-grid-component.jpg",
+        URL:"../portfolio/frontendmentor/single-price-grid-component/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/single-price-grid-component.jpg",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Intro component with sign-up form",
-        URL:"../../../../frontendmentor/intro-component-with-sign-up-form/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/intro-component-with-sign-up-form.png",
+        URL:"../portfolio/frontendmentor/intro-component-with-sign-up-form/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/intro-component-with-sign-up-form.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Base Apparel coming soon page",
-        URL:"../../../../frontendmentor/base-apparel-coming-soon-page/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/base-apparel-coming-soon.png",
+        URL:"../portfolio/frontendmentor/base-apparel-coming-soon-page/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/base-apparel-coming-soon.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Social proof section",
-        URL:"../../../../frontendmentor/social-proof-section/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/social-proof-section.png",
+        URL:"../portfolio/frontendmentor/social-proof-section/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/social-proof-section.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"FAQ accordion card",
-        URL:"../../../../frontendmentor/FAQ-accordion-card/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/FAQ-accordion-card.png",
+        URL:"../portfolio/frontendmentor/FAQ-accordion-card/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/FAQ-accordion-card.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Profile card component",
-        URL:"../../../../frontendmentor/profile-card-component/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/profile-card-component.png",
+        URL:"../portfolio/frontendmentor/profile-card-component/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/profile-card-component.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Preview card component",
-        URL:"../../../../frontendmentor/3-column-preview-card-component/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/3-column-preview-card-component.png",
+        URL:"../portfolio/frontendmentor/3-column-preview-card-component/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/3-column-preview-card-component.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
     },
     {
         name:"Meet Landing Page",
-        URL:"../../../../frontendmentor/meet-landing-page/index.html",
-        thumbnail:"../../../../frontendmentor/thumbnails/meet-landing-page.png",
+        URL:"../portfolio/frontendmentor/meet-landing-page/index.html",
+        thumbnail:"../portfolio/frontendmentor/thumbnails/meet-landing-page.png",
         frontendmentor:true,
         freelance:false,
         difficulty: "newbie"
