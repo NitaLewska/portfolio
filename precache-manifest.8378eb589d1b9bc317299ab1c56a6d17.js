1bf91f2e4c8e407ff9b429b9eae8d800c0758c75
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2022-12-07 01:56:21 +0300

    Deploy React app to GitHub Pages 07/12

--- precache-manifest.8378eb589d1b9bc317299ab1c56a6d17.js
+++ precache-manifest.8378eb589d1b9bc317299ab1c56a6d17.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5807835750c60c294fa3",
-    "url": "/portfolio/static/css/main.6095cd55.chunk.css"
+    "revision": "bd81abe4597da958b99e",
+    "url": "/portfolio/static/css/main.c8e535dc.chunk.css"
   },
   {
-    "revision": "5807835750c60c294fa3",
-    "url": "/portfolio/static/js/main.58078357.chunk.js"
+    "revision": "bd81abe4597da958b99e",
+    "url": "/portfolio/static/js/main.bd81abe4.chunk.js"
   },
   {
     "revision": "6c24f27bba7313b99e26",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "24d392fc2257a6723ffb073a22fb8dc9",
+    "revision": "fe0feceda55136346d5fd5bcccb2c9ec",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.300b472f9770a289fba13233990ddad7.js"
+  "/portfolio/precache-manifest.8378eb589d1b9bc317299ab1c56a6d17.js"
 );
 
 workbox.clientsClaim();
