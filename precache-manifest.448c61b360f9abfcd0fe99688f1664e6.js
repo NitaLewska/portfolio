594a9e77ca71f59c14dcb7bf3236a1919bdb1024
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-01-15 23:23:43 +0300

    Updates

--- precache-manifest.448c61b360f9abfcd0fe99688f1664e6.js
+++ precache-manifest.448c61b360f9abfcd0fe99688f1664e6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "087d74907cedc563adab",
+    "revision": "c26141af66585a07290c",
     "url": "/portfolio/static/css/main.f414a963.chunk.css"
   },
   {
-    "revision": "087d74907cedc563adab",
-    "url": "/portfolio/static/js/main.087d7490.chunk.js"
+    "revision": "c26141af66585a07290c",
+    "url": "/portfolio/static/js/main.c26141af.chunk.js"
   },
   {
     "revision": "7fe47ebaa6fb784ebffe",
@@ -16,39 +16,39 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "809ebbbffdb918545360fa3145e78558",
-    "url": "/portfolio/static/media/elections.809ebbbf.jpg"
+    "revision": "de3c26ad45697d5d64a45f5b480d0d73",
+    "url": "/portfolio/static/media/webdev.de3c26ad.jpeg"
   },
   {
-    "revision": "1aa0d5570fe44e8f692271e14d721ba3",
-    "url": "/portfolio/static/media/pyaterochka.1aa0d557.webp"
+    "revision": "809ebbbffdb918545360fa3145e78558",
+    "url": "/portfolio/static/media/elections.809ebbbf.jpg"
   },
   {
     "revision": "07dd76631c1c32b8667edf88ab347391",
     "url": "/portfolio/static/media/realtor.07dd7663.jpeg"
   },
   {
-    "revision": "de3c26ad45697d5d64a45f5b480d0d73",
-    "url": "/portfolio/static/media/webdev.de3c26ad.jpeg"
-  },
-  {
     "revision": "70086aefc144ef041ec4b6668b02676e",
     "url": "/portfolio/static/media/tutor.70086aef.jpg"
-  },
-  {
-    "revision": "221145690898f61bb66a40498674b643",
-    "url": "/portfolio/static/media/physics.22114569.jpg"
   },
   {
     "revision": "ddc1eebd229843d4165e1a1736da237b",
     "url": "/portfolio/static/media/mts.ddc1eebd.jpg"
   },
   {
+    "revision": "221145690898f61bb66a40498674b643",
+    "url": "/portfolio/static/media/physics.22114569.jpg"
+  },
+  {
     "revision": "f256d9f98558dc8dbe1f907f6d94b318",
     "url": "/portfolio/static/media/incity.f256d9f9.jpg"
   },
   {
-    "revision": "6eb0b47f388d37c50c4e207b43821439",
+    "revision": "1aa0d5570fe44e8f692271e14d721ba3",
+    "url": "/portfolio/static/media/pyaterochka.1aa0d557.webp"
+  },
+  {
+    "revision": "d4bba226f768ff6f1d2848499a4eb6ba",
     "url": "/portfolio/index.html"
   }
 ];
