3f57e5b6b58cfd18740f4dd8d071282925a072de
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-01-17 01:57:34 +0300

    Updates

--- precache-manifest.e71d1bba0f8291e6d4be900a2d87f8d8.js
+++ precache-manifest.e71d1bba0f8291e6d4be900a2d87f8d8.js
@@ -1,39 +1,39 @@
 self.__precacheManifest = [
   {
-    "revision": "4f5631340b7115066055",
-    "url": "/portfolio/static/css/main.d4df85f4.chunk.css"
+    "revision": "0f69c186216936b671ca",
+    "url": "/portfolio/static/css/main.cf58e6be.chunk.css"
   },
   {
-    "revision": "4f5631340b7115066055",
-    "url": "/portfolio/static/js/main.4f563134.chunk.js"
+    "revision": "0f69c186216936b671ca",
+    "url": "/portfolio/static/js/main.0f69c186.chunk.js"
   },
   {
-    "revision": "7fe47ebaa6fb784ebffe",
-    "url": "/portfolio/static/js/1.7fe47eba.chunk.js"
+    "revision": "816170884b3d1c890c87",
+    "url": "/portfolio/static/js/1.81617088.chunk.js"
   },
   {
     "revision": "ee2a100325fd284f0097",
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
+    "revision": "de3c26ad45697d5d64a45f5b480d0d73",
+    "url": "/portfolio/static/media/webdev.de3c26ad.jpeg"
+  },
+  {
     "revision": "07dd76631c1c32b8667edf88ab347391",
     "url": "/portfolio/static/media/realtor.07dd7663.jpeg"
-  },
-  {
-    "revision": "809ebbbffdb918545360fa3145e78558",
-    "url": "/portfolio/static/media/elections.809ebbbf.jpg"
-  },
-  {
-    "revision": "70086aefc144ef041ec4b6668b02676e",
-    "url": "/portfolio/static/media/tutor.70086aef.jpg"
   },
   {
     "revision": "ddc1eebd229843d4165e1a1736da237b",
     "url": "/portfolio/static/media/mts.ddc1eebd.jpg"
   },
   {
-    "revision": "de3c26ad45697d5d64a45f5b480d0d73",
-    "url": "/portfolio/static/media/webdev.de3c26ad.jpeg"
+    "revision": "70086aefc144ef041ec4b6668b02676e",
+    "url": "/portfolio/static/media/tutor.70086aef.jpg"
+  },
+  {
+    "revision": "809ebbbffdb918545360fa3145e78558",
+    "url": "/portfolio/static/media/elections.809ebbbf.jpg"
   },
   {
     "revision": "221145690898f61bb66a40498674b643",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/pyaterochka.1aa0d557.webp"
   },
   {
-    "revision": "b022681d983edce33915f2c705fba0af",
+    "revision": "52b927a7d7a6120bcae17b7871659a64",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.35fcd7be9fe9e1938e5f13dee1117d65.js"
+  "/portfolio/precache-manifest.e71d1bba0f8291e6d4be900a2d87f8d8.js"
 );
 
 workbox.clientsClaim();
