37cd801f14d9ed9e2ebec6ef3576bbf5b713cb85
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-02-06 02:44:52 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b03274788ee28644a4fd58ea94487a02.js"
+  "/portfolio/precache-manifest.c39fa775557af24da802699f1dfe834e.js"
 );
 
 workbox.clientsClaim();
