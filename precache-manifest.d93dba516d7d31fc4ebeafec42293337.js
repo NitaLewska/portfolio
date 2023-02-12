1315f3451bde7e4eb3682f57a8f76ffa8f83333d
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-02-13 00:36:48 +0300

    Updates

--- precache-manifest.d93dba516d7d31fc4ebeafec42293337.js
+++ precache-manifest.d93dba516d7d31fc4ebeafec42293337.js
@@ -16,32 +16,32 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "1aa0d5570fe44e8f692271e14d721ba3",
-    "url": "/portfolio/static/media/pyaterochka.1aa0d557.webp"
-  },
-  {
-    "revision": "0594058000b9188f970dfac4720436c7",
-    "url": "/portfolio/static/media/incity.05940580.jpg"
-  },
-  {
-    "revision": "fd1d577560eb9cc3e7cf48e42a216cd1",
-    "url": "/portfolio/static/media/webdev.fd1d5775.jpeg"
+    "revision": "23a31e76b62e49e59f7bf50a65361675",
+    "url": "/portfolio/static/media/realtor.23a31e76.jpeg"
   },
   {
     "revision": "e43131c389a6999849d3ca02509f7751",
     "url": "/portfolio/static/media/tutor.e43131c3.jpg"
   },
   {
-    "revision": "f4a506655e6e2b295f634cf0631f012d",
-    "url": "/portfolio/static/media/mts.f4a50665.jpg"
+    "revision": "1aa0d5570fe44e8f692271e14d721ba3",
+    "url": "/portfolio/static/media/pyaterochka.1aa0d557.webp"
+  },
+  {
+    "revision": "fd1d577560eb9cc3e7cf48e42a216cd1",
+    "url": "/portfolio/static/media/webdev.fd1d5775.jpeg"
   },
   {
     "revision": "5cb4175fa904aa97d05434ae1d66c871",
     "url": "/portfolio/static/media/elections.5cb4175f.jpg"
   },
   {
-    "revision": "23a31e76b62e49e59f7bf50a65361675",
-    "url": "/portfolio/static/media/realtor.23a31e76.jpeg"
+    "revision": "f4a506655e6e2b295f634cf0631f012d",
+    "url": "/portfolio/static/media/mts.f4a50665.jpg"
+  },
+  {
+    "revision": "0594058000b9188f970dfac4720436c7",
+    "url": "/portfolio/static/media/incity.05940580.jpg"
   },
   {
     "revision": "c6f818f355b2a884da1b25231e84c993",
