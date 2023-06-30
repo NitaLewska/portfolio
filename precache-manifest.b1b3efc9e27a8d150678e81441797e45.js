a381275443fb4c4685df1d6d43030e968442ea10
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-07-01 00:31:26 +0300

    Updates

---
+++ precache-manifest.b1b3efc9e27a8d150678e81441797e45.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "09355d177b0b3eed246c",
+    "url": "/static/css/main.d75c8208.chunk.css"
+  },
+  {
+    "revision": "09355d177b0b3eed246c",
+    "url": "/static/js/main.09355d17.chunk.js"
+  },
+  {
+    "revision": "75f84090a6ac3cb98560",
+    "url": "/static/js/1.75f84090.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23a31e76b62e49e59f7bf50a65361675",
+    "url": "/static/media/realtor.23a31e76.jpeg"
+  },
+  {
+    "revision": "fd1d577560eb9cc3e7cf48e42a216cd1",
+    "url": "/static/media/webdev.fd1d5775.jpeg"
+  },
+  {
+    "revision": "5cb4175fa904aa97d05434ae1d66c871",
+    "url": "/static/media/elections.5cb4175f.jpg"
+  },
+  {
+    "revision": "e43131c389a6999849d3ca02509f7751",
+    "url": "/static/media/tutor.e43131c3.jpg"
+  },
+  {
+    "revision": "0594058000b9188f970dfac4720436c7",
+    "url": "/static/media/incity.05940580.jpg"
+  },
+  {
+    "revision": "f4a506655e6e2b295f634cf0631f012d",
+    "url": "/static/media/mts.f4a50665.jpg"
+  },
+  {
+    "revision": "c6f818f355b2a884da1b25231e84c993",
+    "url": "/static/media/physics.c6f818f3.jpg"
+  },
+  {
+    "revision": "1aa0d5570fe44e8f692271e14d721ba3",
+    "url": "/static/media/pyaterochka.1aa0d557.webp"
+  },
+  {
+    "revision": "066b857bc87d51761847358fb0a18558",
+    "url": "/index.html"
+  }
+];
